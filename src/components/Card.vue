a553207a760b61673b54279d2ec279398e3b0b88
martin <Martin.yu@baiyitech.com.tw>
2023-01-13 17:59:26 +0800

    新增card 元件

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card" :style="position">
+    <img :src="'/src/assets/card/' + name + '.png'" alt="" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { computed } from "vue";
+const props = defineProps<{
+  name: string;
+  index: number;
+}>();
+const distanceX = 150;
+const distanceY = 50;
+
+const position = computed(() => {
+  return {
+    top: (props.index % 8) * distanceY + "px",
+    left: (props.index % 8) * distanceX + "px",
+  };
+});
+</script>
+<style lang="scss" scoped>
+.card {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 137px;
+  height: 189px;
+  color: black;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,15 @@
 <template>
-    test
+  <div class="container">
+    <Card v-for="(_, index) of 9" :index="index" name="spade-1" />
+  </div>
 </template>
-<script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<script lang="ts" setup>
+import Card from "@/components/Card.vue";
+</script>
+<style lang="scss" scoped>
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
